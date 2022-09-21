17c5e821219a84f6ca8fcc620e608556634d0239
ensbstrk <enesbasturk2@gmail.com>
2022-09-21 13:37:10 +0300

    firstCommit

---
+++ veriables/src/veriables/Main.java
@@ -0,0 +1,15 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 11;
+		String mesaj = "Öğrenci Sayısı:";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		System.out.println("Öğrenci sayım : "+ogrenciSayisi);
+		
+	}
+
+}
