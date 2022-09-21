31c28fba0a38aa67bbee3c584646716a061c224d
ensbstrk <enesbasturk2@gmail.com>
2022-09-21 13:30:47 +0300

    firstCommit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 double sayi = 12.6;
+		 sayi = 13;
+		 
+	
+		 char karakter = 'A';
+		 
+		boolean dogruMu = true;
+	}
+
+}
