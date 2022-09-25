81f91e484c42ff4420257512ccd02c6405808af8
ensbstrk <enesbasturk2@gmail.com>
2022-09-25 21:55:49 +0300

    firstCommit

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,16 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+     DortIslem dortIslem = new DortIslem();
+    int sonuc = dortIslem.Topla(3, 4);
+     //dortIslem.Cıkar(0, 0);
+     //dortIslem.Carp(0, 0);
+     //dortIslem.Bol(0, 0);
+     
+     System.out.println(sonuc);
+     
+	}
+
+}
